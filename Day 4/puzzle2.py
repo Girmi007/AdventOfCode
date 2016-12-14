e3c7653e41373cd8d175a495f5236f00f7253450
Girmi <girmi.schouten@student.uantwerpen.be>
2016-12-14 13:06:45 +0100

    Day4.2 complete

--- Day 4/puzzle2.py
+++ Day 4/puzzle2.py
@@ -1,36 +1,35 @@
 import re
 import string
-from itertools import cycle
 from operator import itemgetter
 
 
 class RoomSecurity:
 
     def __init__(self):
-        with open('input_test.txt', 'rt') as textfile:
+        with open('input.txt', 'rt') as textfile:
             self.input = textfile.readlines()
 
-    def get_verified_id(self, encrypted_name):
-        sector_id = re.search("-([0-9]+)\[", encrypted_name).group(1)
+    def decrypt_room_name(self, encrypted_name):
+        sector_id = int(re.search("-([0-9]+)\[", encrypted_name).group(1))
         checksum = re.search("\[([A-Za-z]+)\]", encrypted_name).group(1)
         room_name = re.search("^[A-Za-z\-]+", encrypted_name).group().replace('-', ' ')
-
-        sector_id = int(sector_id)
-        room_name = [self.shift_letter(letter, sector_id) for letter in room_name]
 
         letter_count = [[room_name.count(letter), letter] for letter in string.ascii_lowercase]
         top_letters = sorted(letter_count, reverse=True, key=itemgetter(0))[0:5]
         generated_checksum = ''.join(x[1] for x in top_letters)
 
-        return int(sector_id) if generated_checksum == checksum else 0
+        shifted_room_name = ''.join([self.shift_letter(letter, sector_id) for letter in room_name])
+        return (shifted_room_name, sector_id) if generated_checksum == checksum else None
 
     def shift_letter(self, letter, shift):
-        shifted = chr(((ord(letter) - 64 + shift) % 26) + 64)
-        return shifted
+        shifted = chr(((ord(letter) - 97 + shift) % 26) + 97)
+        return shifted if letter in string.ascii_lowercase else letter
 
     def main(self):
-        id_sum = sum([self.get_verified_id(room_name) for room_name in self.input])
-        print("Sum of IDs: %d" % id_sum)
+        decrypted_names = [self.decrypt_room_name(room_name) for room_name in self.input]
+        decrypted_names = [name for name in decrypted_names if name is not None]
+        north_pole_room = [room_name for room_name in decrypted_names if re.match("north|pole", room_name[0])]
+        print("North pole room: %s, id: %d" % north_pole_room[0])
 
 if __name__ == "__main__":
     puzzle = RoomSecurity()
